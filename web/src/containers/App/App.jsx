b00ac091f290cc70fc7fe906cb94d088eb914d4b
kpkym <okpkym@gmail.com>
2020-03-24 05:28:52 +0800

    使用ant 搭建简单界面

--- web/src/containers/App/App.jsx
+++ web/src/containers/App/App.jsx
@@ -1,31 +1,69 @@
 import React, {Component} from 'react';
-
-import './css/App.css';
-import ProductItem from 'components/ProductItem/ProductItem';
-import {getAllMercari} from 'api/index'
+import {Button, Card, Col, Row, Statistic} from 'antd';
+import {DeleteOutlined, HeartTwoTone} from '@ant-design/icons';
+import "./css/App.css"
+import data from 'test/data.json';
+import {baseImgUrl} from 'config/config'
 
 class App extends Component {
     static propTypes = {};
 
+    constructor(props) {
+        super(props);
+        let mercariMatrix = [], lineLen = 6;
+        let t = [];
+        for (let e of data.data) {
+            if (t.length === lineLen) {
+                mercariMatrix.push(t);
+                t = [];
+            } else {
+                t.push(e);
+            }
+        }
+        if (t.length !== 0) {
+            mercariMatrix.push(t);
+        }
+
+        this.state = {mercariMatrix};
+    }
+
     state = {
-        mercaris: []
+        mercariMatrix: []
 
     };
 
     componentDidMount() {
-        getAllMercari().then(e => this.setState({mercaris: e.data.data}));
+        // getAllMercari().then(e => this.setState({mercaris: e.data.data}));
     }
 
     render() {
         return (
-            <table>
-                <tbody>
-                    {this.state.mercaris.map(e => <ProductItem key={e.url} mercari={e}/>)}
-                </tbody>
-            </table>
-        );
+            <>
+                {this.state.mercariMatrix.map((line, index) => (
+                    <Row gutter={[20, 20]} key={index}>
+                        {line.map(e => (
+                            <Col span={4} key={e.url}>
+                                <Card
+                                    title={e.title}
+                                    hoverable
+                                    cover={<a href={e.url} style={{height: "100%", width: "100%", textAlign:"center"}} target="_blank">
+                                        <img style={{height: "100px", objectFit: 'scale-down'}}
+                                             src={baseImgUrl + "/" + e.pictures[0]}/></a>}
+                                    actions={[
+                                        <Button type="link"><HeartTwoTone twoToneColor="#eb2f96"/></Button>,
+                                        <Button type="link"><HeartTwoTone twoToneColor="#ccc"/></Button>,
+                                        <Button type="link"><DeleteOutlined/></Button>,
+                                    ]}
+                                >
+                                    <Card.Meta title={e.title}
+                                               description={<Statistic title="价格" value={e.currentPrice}/>}/>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                ))}
+            </>);
     }
-
 }
 
 export default App;
--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './containers/App/App';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
