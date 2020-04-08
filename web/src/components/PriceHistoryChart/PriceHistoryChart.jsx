0e158e70100d0658966ab5e268281dec91646967
kpkym <okpkym@gmail.com>
2020-04-08 22:59:54 +0800

    优化缺货时显示

--- web/src/components/PriceHistoryChart/PriceHistoryChart.jsx
+++ web/src/components/PriceHistoryChart/PriceHistoryChart.jsx
@@ -2,12 +2,29 @@ import React, {Component} from 'react';
 import {VictoryChart, VictoryLine, VictoryScatter} from 'victory';
 import {outOfStockPrice} from 'util/utils'
 
+function getPrice(item, arr) {
+    // 不缺货
+    if (item.price !== outOfStockPrice){
+        return item.price;
+    }
+
+    // 缺货，选择前一个价格
+    for (let i = arr.findIndex(e => e === item) - 1; i > -1; i--) {
+        if (arr[i] !== outOfStockPrice) {
+            return arr[i].price;
+        }
+    }
+    // 去掉所有缺货标签后，选择第一个
+    // 如果去掉所有之后为空，则返回0
+    let outOfStockArr = arr.map(e => e.price).filter(e => e !== outOfStockPrice);
+    return outOfStockArr.length > 0 ? outOfStockArr[0] : 0;
+}
+
 class PriceHistoryChart extends Component {
     static propTypes = {};
 
     render() {
         const {chartData} = this.props;
-        let maxVal = Math.max(...chartData.map(e => e.price).filter(e => e !== outOfStockPrice));
         return (
             <VictoryChart width={800} height={150}>
                 <VictoryLine style={{data: {stroke: "#c43a31"}}}
@@ -17,7 +34,7 @@ class PriceHistoryChart extends Component {
                         let date = new Date(e.dateTime);
                         return `${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
                     }}
-                    y={e => e.price === outOfStockPrice ? maxVal : e.price}
+                    y={e => getPrice(e, chartData)}
                 />
                 <VictoryScatter data={chartData}
                                 size={5}
@@ -25,7 +42,7 @@ class PriceHistoryChart extends Component {
                                     let date = new Date(e.dateTime);
                                     return `${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
                                 }}
-                                y={e => e.price === outOfStockPrice ? maxVal : e.price}
+                                y={e => getPrice(e, chartData)}
                                 style={{ data: { fill: "#c43a31" } }}
                 />
             </VictoryChart>
