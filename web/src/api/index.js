439f7cdb3d7c0bb8d33335f73124691228bb69ec
kpkym <okpkym@gmail.com>
2020-05-14 10:54:49 +0800

    特殊字符转义

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,6 +1,8 @@
 import ajax from './ajax'
 
-export const getAllMercari = (keyword) => ajax('mercari/list?' + (keyword ? `keyword=${keyword}` : ''));
+export const getAllMercari = (keyword) => {
+    return ajax('mercari/list?' + (keyword ? `keyword=${window.encodeURIComponent(keyword)}` : ''));
+}
 export const updateMercari = (mercari) => ajax('mercari', mercari, "PUT");
 export const delMercari = (ids) => ajax('mercari', ids, "PATCH");
 export const crawlerMercari = () => ajax('mercari/start');
