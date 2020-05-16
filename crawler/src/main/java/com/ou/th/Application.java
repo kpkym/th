5096e8fbde11336a1179c13020197493d048615f
kpkym <okpkym@gmail.com>
2020-05-17 01:00:50 +0800

    上次下载图片失败时，这次获取项目时重新下载图片

--- crawler/src/main/java/com/ou/th/Application.java
+++ crawler/src/main/java/com/ou/th/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 // 解决jmx重复注册bean的问题
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @EnableScheduling
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
         System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
--- crawler/src/main/java/com/ou/th/controller/SurugayaController.java
+++ crawler/src/main/java/com/ou/th/controller/SurugayaController.java
@@ -4,12 +4,15 @@ import cn.hutool.core.util.StrUtil;
 import com.ou.th.crawler.SurugayaScrawler;
 import com.ou.th.crawler.surugaya.SurugayaModel;
 import com.ou.th.crawler.surugaya.SurugayaService;
+import com.ou.th.util.FastdfsUtil;
 import com.ou.th.util.Msg;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author kpkym
@@ -24,6 +27,9 @@ public class SurugayaController {
     @Autowired
     SurugayaService surugayaService;
 
+    @Autowired
+    FastdfsUtil fastdfsUtil;
+
     @GetMapping(value = "start")
     public void surugaya() throws IOException {
         surugayaScrawler.start();
@@ -31,7 +37,17 @@ public class SurugayaController {
 
     @GetMapping("list")
     public Msg list(String keyword) {
-        return Msg.success(StrUtil.isEmpty(keyword) ? surugayaService.list() : surugayaService.list(keyword));
+        List<SurugayaModel> surugayaModels = StrUtil.isEmpty(keyword) ? surugayaService.list() : surugayaService.list(keyword);
+        flushImg(surugayaModels);
+        return Msg.success(surugayaModels);
+    }
+
+    @Async
+    public void flushImg(List<SurugayaModel> surugayaModels) {
+        List<SurugayaModel> models = surugayaModels.stream().filter(e -> e.getPicture().contains("https://"))
+                .peek(e -> e.setPicture(fastdfsUtil.uploadFromUrl(e.getPicturesOriginal())))
+                .collect(Collectors.toList());
+        surugayaService.save(models);
     }
 
     @PutMapping("")
