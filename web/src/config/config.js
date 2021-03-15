6e06055d5d608e6b599fbfffe6617866d08a7f7e
kpkym <okpkym@gmail.com>
2021-03-15 22:43:41 +0800

    移动文件

--- web/src/config/config.js
+++
@@ -1,2 +0,0 @@
-export let baseUrl = "http://localhost:8688/";
-export let baseImgUrl = "http://localhost:57596/";
--- web/src/redux/action-types.js
+++
@@ -1,8 +0,0 @@
-/*
-包含n个action type名称常量
- */
-export const INIT = 'INIT';
-export const UPDATE = 'UPDATE';
-export const UPDATE_SINGLE = 'UPDATE_SINGLE';
-export const DELETE = 'DELETE';
-export const SEARCH = 'SEARCH';
